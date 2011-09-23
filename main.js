1edf8f02299572e2318e62d474fa551ca6100f3e
anky <ankurdnana@gmail.com>
2011-09-23 17:39:18 +0530

    added keyboard shortcut support
    Made functions slide_in() and slide_out()

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
-$('#arrow').click(function(){
+$('#arrow').click(slide_out);
 
+function slide_out(){
 $('#arrow_close').css("display","block"); 
 $('#side').animate({width:300},300);
 $('.post_info').animate({opacity:0},300);
@@ -14,12 +15,12 @@ $('#side_cat').css("display","block");
 $('#side_title').css("display","block");
 $('#side_logo img').css("display","block");
 $('.side_content').css("display","block");
-
 },300);
 
-});
+};
 
-$('#arrow_close').click(function(){
+$('#arrow_close').click(slide_in);
+function slide_in(){
 $('#side').animate({width:30},300); 
 $('#header').animate({marginLeft:20},300);
 $('#header').animate({opacity:1},300); 
@@ -36,7 +37,31 @@ $('#side_logo img').css("display","none");
 $('.side_content').css("display","none");
 
     
-    });
+    };
 
+   $(document).keyup(function(e) {
+      switch(e.keyCode) { 
+         // User pressed "up" arrow
+        case 37:
+                 event.preventDefault();
+
+        slide_in();
+        break;
+         // User pressed "down" arrow
+         case 39:
+                 event.preventDefault();
+
+            slide_out();
+         break;
+         // User pressed "enter"
+         case 27:
+                 event.preventDefault();
+
+            slide_in();
+         break;
+      }
+   });
 
 });
+
+
